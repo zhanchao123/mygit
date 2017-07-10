6e58a2ed719eb49cf29e83591dd65d3b0e69fb70
zhanchao123 <626039409@qq.com>
2017-07-10 23:16:43 +0800

    ddd

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * Author:ZhanChao
+ * Date:  2017-07-10
+ * Time:  18:23
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("this is a test");
+    }
+}
