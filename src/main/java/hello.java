3dfdbb81ed53d0c92daee3739f185cf4bc6efee0
zhanchao123 <626039409@qq.com>
2017-07-10 18:07:16 +0800

    first commit

---
+++ src/main/java/hello.java
@@ -0,0 +1,10 @@
+/**
+ * Author:ZhanChao
+ * Date:  2017-07-10
+ * Time:  17:06
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("1111111111111");
+    }
+}
